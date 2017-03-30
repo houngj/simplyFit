25193fdc7f58615b2283b8f9b598d068235a62b6
houngj <sjhoung@gmail.com>
2017-03-30 06:11:25 -0400

    Add simple testcase for CI testing

--- simply_fit/simply_fit_api/models.py
+++ simply_fit/simply_fit_api/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class utils(object):
+	def __init__(self):
+		self.tmp = None
+	def test(self):
+		return True
--- simply_fit/simply_fit_api/tests.py
+++ simply_fit/simply_fit_api/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import utils
 
 # Create your tests here.
+class basicTest(TestCase):
+	def test_for_true(self):
+		myUtils = utils()
+		self.assertIs(myUtils.test(), True)
